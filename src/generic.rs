96a2793af481bfd985e2627dc073c9cda72f31e9
Ofuzor chukwuemeke <uniben2018@gmail.com>
2024-12-16 15:57:04 +0100

    updated

--- src/generic.rs
+++ src/generic.rs
@@ -1,11 +1,32 @@
 // Generics Examples
 
+use std::fmt::{Debug, Display};
 
-fn return_thing<T>(thing:T)->T{
-    println!("Here is a Thing");
+#[derive(Debug)]
+struct Animal {
+    name: String,
+    age: u8,
 }
 
-fn caller(){
-    let my_string = return_thing(String::from("I am a string"));
-    return my_string
+fn compare_and_display<T, U>(statement: T, num_1: U, num2: U)
+where
+    T: Display,
+    U:Display + PartialOrd
+{
+}
+
+fn print_item<T: Debug + Display>(item: T) {
+    println!("Here is yout item {:?}", item);
+}
+
+fn main() {
+    let charlie = Animal {
+        name: String::from("Charlie"),
+        age: 1,
+    };
+
+    let number = 55;
+
+    print_item(charlie);
+    print_item(number);
 }
